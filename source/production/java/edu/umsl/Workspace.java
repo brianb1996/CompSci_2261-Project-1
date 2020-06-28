90dbb7bb8efdbf86935da76d10432094dba11b72
Brian <babcnb@umsystem.edu>
2020-06-28 17:03:02 -0500

    Added name, title and date to program

--- source/production/java/edu/umsl/Workspace.java
+++ source/production/java/edu/umsl/Workspace.java
@@ -1,32 +1,36 @@
+//Brian Bredahl
+//Project 1
+//June 23,2020
+
 package edu.umsl;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 /*
-* Project 1 - CMPSCI 2261
-*
-* Every school morning Steve is woken up by the sound of his alarm clock. Since he is a bit forgetful, quite often he
-* leaves the alarm on Saturday morning too. That’s not too bad though, since he feels good when he realizes he doesn’t
-* have to get up from his warm and cozy bed.
-*
-* He likes that so much that he would like to experience that on other days of the week too! His friend Justin offered
-* this simple solution: set his alarm clock 45 minutes early, and he can enjoy the comfort of his bed, fully awake, for
-* 45 minutes each day.
-*
-* Steve decided to heed Justin’s advice, however Steve’s alarm clock uses 24-hour notation and Steve has issues with
-* adjusting the time. Help Steve and write a program that will take one time stamp, in 24-hour notation, and print out a
-* new time stamp, 45 minutes earlier, also in 24-hour notation.
-*
-* If you are unfamiliar with 24-hour time notation yourself, you might be interested to know it starts with 0:00
-* (midnight) and ends with 23:59 (one minute before midnight).
-*
-* Input:
-* The first and only line of input will contain exactly two integers H and M (0 ≤ H ≤ 23, 0 ≤ M ≤ 59) separated by a
-* single space, the input time in 24-hour notation. H denotes hours and M minutes. Ensure your program does not
-* terminate if there is bad input and can recover gracefully.
-*
-*
-* */
+ * Project 1 - CMPSCI 2261
+ *
+ * Every school morning Steve is woken up by the sound of his alarm clock. Since he is a bit forgetful, quite often he
+ * leaves the alarm on Saturday morning too. That’s not too bad though, since he feels good when he realizes he doesn’t
+ * have to get up from his warm and cozy bed.
+ *
+ * He likes that so much that he would like to experience that on other days of the week too! His friend Justin offered
+ * this simple solution: set his alarm clock 45 minutes early, and he can enjoy the comfort of his bed, fully awake, for
+ * 45 minutes each day.
+ *
+ * Steve decided to heed Justin’s advice, however Steve’s alarm clock uses 24-hour notation and Steve has issues with
+ * adjusting the time. Help Steve and write a program that will take one time stamp, in 24-hour notation, and print out a
+ * new time stamp, 45 minutes earlier, also in 24-hour notation.
+ *
+ * If you are unfamiliar with 24-hour time notation yourself, you might be interested to know it starts with 0:00
+ * (midnight) and ends with 23:59 (one minute before midnight).
+ *
+ * Input:
+ * The first and only line of input will contain exactly two integers H and M (0 ≤ H ≤ 23, 0 ≤ M ≤ 59) separated by a
+ * single space, the input time in 24-hour notation. H denotes hours and M minutes. Ensure your program does not
+ * terminate if there is bad input and can recover gracefully.
+ *
+ *
+ * */
 
 
 public class Workspace {
